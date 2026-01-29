4f47bf2bcbe2a6158e9050190a923d52499ab341
Hammes <mhammes@concent.com.br>
2026-01-29 12:49:33 -0300

    bubble_sort

---
+++ algorithms/sorting/bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        swapped = False
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return arr
+
+print(bubble_sort([5, 2, 1, 6, 3, 9, 4, 8, 7]))
