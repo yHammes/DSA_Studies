dbdecf7350fb75acaa234f4d162c1f8ae99030f4
Hammes <127154896+yHammes@users.noreply.github.com>
2026-08-25 19:46:47 -0300

    Update Queue.java

--- data_structures/Queue/Queue.java
+++ data_structures/Queue/Queue.java
@@ -1,10 +1,12 @@
 public class Queue {
     private Node front;
     private Node rear;
+    private int size;
 
     public Queue() {
         this.front = null;
         this.rear = null;
+        this.size = 0;
     }
 
     public void enqueue(int value) {
@@ -16,20 +18,31 @@ public class Queue {
             this.rear.next = newNode;
             this.rear = newNode;
         }
+        this.size++;
     }
 
     public Integer dequeue() {
-        if (this.front == null) {
+        if (this.isEmpty()) {
             return null;
         }
         int value = this.front.value;
 
         this.front = this.front.next;
-        if (this.front == null) {
+        if (this.isEmpty()) {
             this.rear = null;
         }
 
+
+        this.size--;
         return value;
+    }
+
+    public boolean isEmpty() {
+        return this.front == null;
+    }
+
+    public int size() {
+        return this.size;
     }
 
     public static void main(String[] args) {
@@ -37,6 +50,7 @@ public class Queue {
         queue.enqueue(1);
         queue.enqueue(2);
         queue.enqueue(3);
+        System.out.println("Size: " + queue.size());
         Integer value = queue.dequeue();
         value = queue.dequeue();
         value = queue.dequeue();
